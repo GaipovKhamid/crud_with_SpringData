fa158ded928a0efe8730261a016d6a7a78d820bf
Khamid Gaipov <gaipovkhamid65@gmail.com>
2024-12-30 22:53:23 +0500

    first

--- src/main/java/com/khamid/first_data/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.khamid.first_data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
-
-}
